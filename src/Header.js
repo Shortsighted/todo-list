efe0e2c8b6632c2dfa32293bcbda33746b71e479
Shortsighted <onlyforyoumyhoney@gmail.com>
2022-12-10 23:07:41 +0400

    Final v1.1

--- src/Header.js
+++ src/Header.js
@@ -7,10 +7,20 @@ export default class Header extends Component {
             <header className={styles.mainBox} onSubmit={this.props.onSubmit}>
                 <h2>To Do List</h2>
                 <p>Total of {this.props.taskCount} tasks</p>
-                <form className={styles.taskCreationBox}>
-                    <input type='text' name='taskContent' required='required' />
-                    <button>Create</button>
-                </form >
+                <div className={styles.taskCreationContainer}>
+                    <form className={styles.taskCreationBox}>
+                        <input type='text' name='taskContent' required='required' />
+                        <button>Create</button>
+                    </form >
+                    <div className={styles.functionsBox}>
+                        <div className={styles.deployTasks}
+                            onClick={this.props.deployTasks}
+                        >↓ Deploy tasks from api ↓</div>
+                        <div className={styles.deleteAllTasks}
+                             onClick={this.props.deleteAllTasks}
+                        >↓ Delete all tasks ↓</div>
+                    </div>
+                </div>
             </header>
         )
     }
